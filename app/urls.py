a1e453c75fedfd354eb2c53189e86e354af2f41e
NicatDev <nicat254memmedov@gmail.com>
2023-11-13 00:18:34 +0400

    22

--- app/urls.py
+++ app/urls.py
@@ -3,13 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='home'),
-    path('bloq',blog,name='blog'),
+    path('article',blog,name='blog'),
     path('portfolio',portfolio,name='portfolio'),
     path('services',services,name='services'),
     path('contact',contact,name='contact'),
     path('message',message,name='message'),
     path('about',about,name='about'),
-    path('bloq/<slug>',blogsingle,name='blogsingle'),
+    path('article/<slug>',blogsingle,name='blogsingle'),
     path('service/<slug>',servicesingle,name='servicesingle'),
     path('portfolio/<slug>',portfoliosingle,name='portfoliosingle'),
 ]
